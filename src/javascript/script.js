93d0d45ffd644cb17b177399a03ebda0f5f632e4
mariaguirelli <eduardaguirelli89@gmail.com>
2025-11-03 18:50:20 -0300

    alterando css para boostrap

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,60 +1,38 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+$( document ).ready(function() {
+  
+    // scroll para as seções
+  
+    let navBtn = $('.nav-item');
+  
+    let homeSection = $('#home-menu');
+    let palestrantesSection = $('#palestrantes-menu');
+    let apoiadoresSection = $('#apoiadores-menu');
+    let inscricaoSection = $('#inscricao-menu');
+    let faleConoscoSection = $('#faleConosco-menu');
+  
+    let scrollTo = '';
+  
+    $(navBtn).click(function() {
+  
+      let btnId = $(this).attr('id');
+  
+      if(btnId == 'home-menu') {
+        scrollTo = homeSection;
+      } else if(btnId == 'palestrantes-menu') {
+        scrollTo = palestrantesSection;
+      } else if(btnId == 'apoiadores-menu') {
+        scrollTo = apoiadoresSection;
+      } else if(btnId == 'inscricao-menu') {
+        scrollTo = inscricaoSection;
+      } else if(btnId == 'faleConosco-menu') {
+        scrollTo = faleConoscoSection;
+      } else {
+        scrollTo = homeSection;
+      }
+  
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $(scrollTo).offset().top - 70
+      }, 1500);
     });
-
-    const sections = $('section');
-    const navItems = $('.nav-item');
-
-    $(window).on('scroll', function () {
-        const header = $('header');
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
-        let activeSectionIndex = 0;
-
-        if (scrollPosition <= 0) {
-            header.css('box-shadow', 'none');
-        } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
-        }
-
-        sections.each(function(i) {
-            const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerHeight();
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeSectionIndex = i;
-                return false;
-            }
-        })
-
-        navItems.removeClass('active');
-        $(navItems[activeSectionIndex]).addClass('active');
-    });
-
-    ScrollReveal().reveal('#cta', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
-
-    ScrollReveal().reveal('.dish', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
-
-    ScrollReveal().reveal('#testimonial_chef', {
-        origin: 'left',
-        duration: 1000,
-        distance: '20%'
-    })
-
-    ScrollReveal().reveal('.feedback', {
-        origin: 'right',
-        duration: 1000,
-        distance: '20%'
-    })
-});
+  
+  });
